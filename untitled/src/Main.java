31f1349dc2cf44d9d00d330c3c25f05c2b792b35
Nadifa Omar <nadifayomar@gmail.com>
2024-11-18 13:46:41 -0800

    made method

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,11 @@
-import java.util.*
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // make methods for cvv, expiration date, credit card number, cardholder name FIRST LAST
+
+    }
+
+    public static boolean creditNumber(){
 
     }
 }
