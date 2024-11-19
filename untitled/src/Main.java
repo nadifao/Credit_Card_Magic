a94d156acd139218d913ac5ecc7707991674c266
Nadifa Omar <nadifayomar@gmail.com>
2024-11-19 12:23:55 -0800

    added more method headers

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,23 @@
-import java.util.*
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // make methods for cvv, expiration date, credit card number, cardholder name FIRST LAST
+
+    }
+
+    public static boolean cvvCheck(){
+
+    }
+
+    public static boolean expirationDate(){
+
+    }
+
+    public static boolean cardNumber(){
+
+    }
+
+    public static boolean cardHolderName(){
 
     }
 }
