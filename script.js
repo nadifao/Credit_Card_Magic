2e32e84f50f65e26c61a944456a6cb7a309a1d9d
Athena Ginis <aginis25@stu.aberdeenhall.com>
2024-11-22 12:11:42 -0800

    fixed exp date

--- script.js
+++ script.js
@@ -1,20 +1,72 @@
 
+let count = 0;
+
 document.getElementById('donation-form').addEventListener('submit', function(event) {
     event.preventDefault();
     let valid = true;
-    
+
     const cardRegex = /^(4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$/;
-    
+
     const cardInput = document.getElementById('credit-card');
     if (!cardRegex.test(cardInput.value)) {
-        document.getElementById('error-message').style.display = 'block';
+        document.getElementById('card-error-message').style.display = 'block';
         valid = false;
     } else {
-        document.getElementById('error-message').style.display = 'none';
+      count++;
+        document.getElementById('card-error-message').style.display = 'none';
     }
-    
-    if (valid) {
+
+    if (count >= 2) {
         alert('Thank you!');
     }
+
+
 });
 
+document.getElementById('cvv-number').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let valid = true;
+
+    const cardRegex = /^[1-9][0-9]{2}$/;
+
+    const cardInput = document.getElementById('cvv');
+    if (!cardRegex.test(cardInput.value)) {
+        document.getElementById('cvv-error-message').style.display = 'block';
+        valid = false;
+    } else {
+      count ++;
+        document.getElementById('cvv-error-message').style.display = 'none';
+        if (count >= 2) {
+            alert('Thank you!');
+        }
+    }
+
+
+});
+
+
+
+document.getElementById('expiration date').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let valid = true;
+
+    const monthRegex = /^1[0-2][/]2[4-9]$|^0[1-9][/]2[4-9]$/;
+
+    const cardInput = document.getElementById('cvv');
+    if (!monthRegex.test(cardInput.value)) {
+        document.getElementById('cvv-error-message').style.display = 'block';
+        valid = false;
+    } else {
+      count ++;
+        document.getElementById('cvv-error-message').style.display = 'none';
+        if (count >= 2) {
+            alert('Thank you!');
+        }
+    }
+
+
+});
+
+// if (count >= 1) {
+//     alert('Thank you!');
+// }
